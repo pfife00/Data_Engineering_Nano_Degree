9fc0dbdf3a2fd04deaf4eb348435351dbf362c79
Forest Pfeiffer <forest.pfeiffer@gmail.com>
2019-08-31 16:21:20 -0700

    Update helper.py
    update clean demo code

--- 6-Project_DEND_Capstone/helper.py
+++ 6-Project_DEND_Capstone/helper.py
@@ -30,6 +30,38 @@ def clean_i94_data(df, i94port_valid):
 
     return df
 
+def check_pop_data(df):
+    """
+    check demographics data for invalid population entries
+    """
+    i=0
+    for index, row in df.iterrows():
+        #if sum of males and females is greater than total population, then there is an issue with the data
+        if(row['Male Population'] + row['Female Population'] != row['Total Population']):
+           print("Issue with number of males or females are row: ", row)
+           print(i)
+
+            #if number of veterans or the number of foreign-born people are greater than total population, then there is an issue with the data
+        elif(row['Number of Veterans'] > row['Total Population'] or row['Foreign-born'] > row['Total Population']):
+           print("Issue with number of foreign born or number of veterans at row: ", row)
+           print(i)
+        i+=1
+
+def clean_demo_data(df):
+    """
+    remove unecesary columns from data
+    """
+    #specify columns to drop (gender columns)
+    drop_cols = ['Male Population', 'Female Population']
+
+    #drop those values in the Male Population and Female Population columns which contain NaNs
+    df = df.dropna(subset=drop_cols)
+
+    #use pandas to rename method to rename State Code to State_Code
+    df.rename(columns={'State Code':'State_Code'}, inplace=True)
+
+    return df
+
 def quality_check(df, description):
     """
     Take dataframe as input and print out result of data quality check.
@@ -49,7 +81,7 @@ def get_i94port_valid(file):
     re_obj = re.compile(r'\'(.*)\'.*\'(.*)\'')
     i94port_valid = {}
     with open(file) as f:
-         for line in f:
-             match = re_obj.search(line)
-             i94port_valid[match[1]]=[match[2]]
+        for line in f:
+            match = re_obj.search(line)
+            i94port_valid[match[1]]=[match[2]]
     return i94port_valid
